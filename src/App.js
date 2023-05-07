fb98a0cca520d5fe255f5a038aa77dbfb0152192
ChBrus <cbruno.carrillo@gmail.com>
2023-05-06 22:03:38 -0600

    Ya lo resolví XD

--- src/App.js
+++ src/App.js
@@ -7,21 +7,8 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hola mamá, ya sé utilizar react :D
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <div className='App-msg'>
-          <p>
-            Hola mamá, ya sé utilizar react :D
-          </p>
-        </div>
       </header>
     </div>
   );
